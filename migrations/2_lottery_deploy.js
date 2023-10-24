c74178b216c244c47359a66a5a0f8f5a5241803c
moasadi <m.asadi.dev@gmail.com>
2023-10-24 15:40:38 +0330

    fix solidity version for work with ganache

--- migrations/2_lottery_deploy.js
+++ migrations/2_lottery_deploy.js
@@ -1,5 +1,4 @@
 const Lottery = artifacts.require("Lottery");
 module.exports = function(deployer,network,accounts) {
-    deployer.deploy(Lottery,accounts[0]);
-
+    deployer.deploy(Lottery);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,7 @@ module.exports = {
     mocha: {},
     compilers: {
         solc: {
-            version: "0.5.1"
+            version: "0.8.13"
     
         }
     }
